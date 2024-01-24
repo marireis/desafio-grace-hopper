237fc4b9ba057afcdbcc5928b2fd3c89675050f5
vhallak <vanessa_hallak@hotmail.com>
2024-01-24 14:03:51 -0300

    questao1.py

--- Desafio 2 - Tomada de decisão/questao1.py
+++ Desafio 2 - Tomada de decisão/questao1.py
@@ -1,6 +1,6 @@
 '''Faça um Programa que peça dois números e imprima o maior deles.'''
 
-def find_max(a, b):
+def comparar(a, b):
     if a > b:
         return a
     else:
@@ -9,5 +9,5 @@ def find_max(a, b):
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 
-max_num = find_max(num1, num2)
-print("O maior número é: ", max_num)
+maior_num = comparar(num1, num2)
+print("O maior número é: ", maior_num)
